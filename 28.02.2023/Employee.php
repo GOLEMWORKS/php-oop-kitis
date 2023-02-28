9d8543b430c60ceced6e402f44e92a2e955ffd10
Vladimir <97763002+GOLEMWORKS@users.noreply.github.com>
2023-02-28 11:31:43 +0200

    1

--- 28.02.2023/Employee.php
+++ 28.02.2023/Employee.php
@@ -6,13 +6,32 @@ class Employee {
 }
 
 $employe1 = new Employee;
-$employee1 -> name = 'John';
-$employee1 -> age = 26;
-$employee1 -> salary = 1000;
+$employe1->name = 'John';
+$employe1->age = 26;
+$employe1->salary = 1000;
 
 $employe2 = new Employee;
-$employee2 -> name = 'Eric';
-$employee2 -> age = 25;
-$employee2 -> salary = 2000;
+$employe2->name = 'Eric';
+$employe2->age = 25;
+$employe2->salary = 2000;
 
-echo( $employee1->salary + $employee2->salary);
+echo( $employe1->salary + $employe2->salary."</br>");
+
+class User
+	{
+		public $name;
+		public $age;
+	}
+	
+	// Первый объект
+	$user1 = new User; // создаем первый объект
+	$user1->name = 'john'; // записываем имя
+	$user1->age = 25; // записываем возраст
+	
+	// Второй объект
+	$user2 = new User; // создаем второй объект
+	$user2->name = 'eric'; // записываем имя
+	$user2->age = 30; // записываем возраст
+	
+	// Найдем сумму возрастов:
+	echo $user1->age + $user2->age; // выведет 55
